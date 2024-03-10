5360597223240ab6f68600ca11d4379990a63088
gitlG <1637885653@qq.com>
2024-03-10 18:48:43 +0800

    更新版本

--- app.py
+++ app.py
@@ -10,9 +10,16 @@ app.register_blueprint(api)
 app.config['SECRET_KEY'] = 'nemo'
 app.permanent_session_lifetime = timedelta(seconds=10) # 设置会话过期时间为40s
 
-
+#版本3
 class Check():
     allowed_routes = ['/login', '/register','/api/adduser','/api/login']
+    _instance = None
+    
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(Check, cls).__new__(cls)
+        return cls._instance
+    
     def check(self,path):
         if path in self.allowed_routes:
             return False
@@ -20,20 +27,49 @@ class Check():
             return False
         
         return True
-                        
-check = Check()    
+"""
+版本2
+class Check():
+    allowed_routes = ['/login', '/register','/api/adduser','/api/login']
+  
+    def check(self,path):
+        if path in self.allowed_routes:
+            return False
+        if path.startswith('/static'):
+            return False
         
-    
+        return True
+check = Check()
 
-# 使用before_request钩子来检查用户是否登录
+"""                      
+
+"""
+版本1
 @app.before_request
 def require_login():
+    path = request.path
+    allowed_routes = ['/login', '/register','/api/adduser','/api/login']
+    if (not path in self.allowed_routes) and not path.startswith('/static')
+        is_login  = session.get("is_login",None)
+        if not is_login :
+            # 用户未登录，重定向到登录页面
+            return render_template('login.html')
+
+"""
+
+        
+# 使用before_request钩子来检查用户是否登录，
+# 该函数返回None则表示继续执行接下来的请求，但是返回值不能布尔值
+@app.before_request
+def require_login():
+    check = Check()    
     # 定义不需要登录就可以访问的路径
     if check.check(request.path):
         is_login  = session.get("is_login",None)
         if not is_login :
             # 用户未登录，重定向到登录页面
             return render_template('login.html')
+    return None
 
 @app.route('/')
 @app.route('/index')
@@ -42,7 +78,6 @@ def index():
     return render_template('index.html')
     
   
-
 @app.route('/login')
 def login():
     return render_template('login.html')
